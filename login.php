aef91277919d71a45d100a6afdc9843cd0aaa755
manuel <josemanuelgrandebautista@gmail.com>
2024-05-20 19:57:27 -0600

    logins

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Usuario</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -48,12 +48,12 @@
 <body>
     <div class="login-container">
         <h2>Login</h2>
-        <form action="valuelogin.php" method="post">
+        <form action="/login" method="post">
             <label for="username">Username</label>
             <input type="text" id="username" name="username" required>
             <label for="password">Password</label>
             <input type="password" id="password" name="password" required>
-            <input type="submit" value="Login">
+            <input type="submit" value="Login"><a href="valuelogin.php?">
         </form>
     </div>
 </body>
